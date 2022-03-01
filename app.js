b59a264b2cf63a403a475700fbda13a5c3bc8048
ngocyen0709 <yenb1906811@student.ctu.edu.vn>
2022-03-01 21:42:41 +0700

    cai dat xu li loi

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const cors = require("cors");
 
 const app = express();
 const setupContactRoutes = require("./app/routes/contact.routes");
+const { BadRequestError, errorHandler } = require("./app/errors");
+const app = express();
 app.use(cors());
 
 app.use(express.json());
@@ -12,4 +14,18 @@ app.get("/", (req, res) => {
 res.json({ message: "Welcome to contact book application." });
 });
 setupContactRoutes(app);
+setupContactRoutes(app);
+// handle 404 response
+app.use((req, res, next) => {
+// Code ở đây sẽ chạy khi không có route được định nghĩa nào
+// khớp với yêu cầu. Gọi next() để chuyển sang middleware xử lý lỗi
+next(new BadRequestError(404, "Resource not found"));
+});
+// define error-handling middleware last, after other app.use() and routes calls
+app.use((err, req, res, next) => {
+// Middleware xử lý lỗi tập trung.
+// Trong các đoạn code xử lý ở các route, gọi next(error)
+// sẽ chuyển về middleware xử lý lỗi này
+errorHandler.handleError(error, res);
 module.exports = app;
+});
